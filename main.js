2d526e2ea79b80bd44fae76e4bf8dca3a4cfae8a
mikkiiottyo <michaelaguirre15@gmail.com>
2025-02-18 18:00:37 -0800

    created a DOMContent and defined init function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 let APIKEY = "5jnRoiokTcyGqEnGr3yOtNiEMFyMwV5l"; 
+
+document.addEventListener("DOMContentLoaded" , init);
+function init() {
+    document.getElementById("btnSearch").addEventListener("click", ev => {
+        ev.preventDefault();
+    })
+}
