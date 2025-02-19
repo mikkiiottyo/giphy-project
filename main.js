536c5475e071fd28986ca15a518cfa9166c09f5c
mikkiiottyo <michaelaguirre15@gmail.com>
2025-02-18 18:17:04 -0800

    create a search endpoint/ give the search input value

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ document.addEventListener("DOMContentLoaded" , init);
 function init() {
     document.getElementById("btnSearch").addEventListener("click", ev => {
         ev.preventDefault();
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+        let str = document.getElementById("search").value.trim();
+        url = url.concat(str);
+        console.log(url);
     })
 }
