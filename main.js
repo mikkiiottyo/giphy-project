722dd7141d94967e8541ea43779588e0fbc438f6
mikkiiottyo <michaelaguirre15@gmail.com>
2025-02-18 18:25:06 -0800

    create a fetch, then respone, then to handle data and catch for errors

--- main.js
+++ main.js
@@ -8,5 +8,14 @@ function init() {
         let str = document.getElementById("search").value.trim();
         url = url.concat(str);
         console.log(url);
-    })
+        fetch(url)
+        .then(response => response.json() )
+        .then(content => {
+            console.log(content.data)
+            console.log('META' , content.meta);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    });
 }
